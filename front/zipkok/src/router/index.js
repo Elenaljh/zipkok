bdc83d587487f7682a48458b9d0a33549a60e04a
ray <yr5698@gmail.com>
2024-05-09 23:36:30 +0900

    feat: init board

--- front/zipkok/src/router/index.js
+++ front/zipkok/src/router/index.js
@@ -9,7 +9,34 @@ const router = createRouter({
       name: 'main',
       component: MainView
     },
-    
+    {
+      path: '/board',
+      name: 'board',
+      component: () => import("@/views/BoardView.vue"),
+      redirect: { name: "article-list" },
+      children: [
+        {
+          path: "list",
+          name: "article-list",
+          component: () => import("@/components/board/BoardList.vue"),
+        },
+        {
+          path: "view/:articleno",
+          name: "article-view",
+          component: () => import("@/components/board/BoardDetail.vue"),
+        },
+        {
+          path: "write",
+          name: "article-write",
+          component: () => import("@/components/board/BoardCreate.vue"),
+        },
+        {
+          path: "modify/:articleno",
+          name: "article-modify",
+          component: () => import("@/components/board/BoardUpdate.vue"),
+        },
+      ],
+    },
   ]
 })
 
---
+++ front/zipkok/src/views/BoardView.vue
@@ -0,0 +1,12 @@
+<script setup></script>
+
+<template>
+  <div class="container text-center mt-3">
+    <div class="alert alert-info" role="alert">게시판</div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<style>
+
+</style>
