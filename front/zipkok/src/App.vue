2754fb9e3233546a631cb817c13d3de1b0e5565d
Elenaljh <hye9807@naver.com>
2024-05-10 11:07:49 +0900

    footer 제작

--- front/zipkok/src/App.vue
+++ front/zipkok/src/App.vue
@@ -1,12 +1,14 @@
 <script setup>
 import HeaderNavBar from "./components/layout/HeaderNavBar.vue";
+import FooterNavBar from "./components/layout/FooterNavBar.vue";
 </script>
 
 <template>
-  <HeaderNavBar />
-  <RouterView/>
+  <body class="d-flex flex-column min-vh-100">
+    <HeaderNavBar />
+    <RouterView />
+    <FooterNavBar class="mt-auto" />
+  </body>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
