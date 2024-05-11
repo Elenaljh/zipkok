ae025a5ca853e4d1abed20aa63e2d67011a8baf2
Jihye Lee <zerom980706@gmail.com>
2024-05-11 23:59:36 +0900

    feat: 최신뉴스 페이지 디자인 완성

--- front/zipkok/src/components/house/detail/item/HouseDetailFacItem.vue
+++ front/zipkok/src/components/house/detail/item/HouseDetailFacItem.vue
@@ -1,7 +1,158 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+
+const dong = ref("진평동");
+const population = ref({ age: 20, sex: "남성", pop: "1,000", ratio: 10.5 });
+const office = ref({
+  name: "진평경찰서",
+  type: "치안시설",
+  address: "경상북도 구미시 진평동",
+});
+const school = ref({
+  name: "진평초등학교",
+  type: "초등학교",
+  address: "경상북도 구미시",
+});
+const restaurant = ref({
+  name: "진평순두부",
+  type: "한식",
+  address: "경상북도 구미시 어쩌고",
+});
+const cctv = ref("경상북도 구미시 인동34길 22, 이디야커피(진평동)");
+</script>
 
 <template>
-  <div>관공서정보입니다.</div>
+  <div class="me-2">
+    <!--인구정보-->
+    <div>
+      <p style="font-size: large; font-weight: bold">
+        <span style="color: #00b3d6">{{ dong }}</span
+        ><span>의 인구구조</span>
+      </p>
+      <div
+        class="table-responsive card"
+        style="max-height: 300px; overflow-y: auto"
+      >
+        <table class="table mb-0">
+          <thead class="table-light">
+            <tr>
+              <th scope="col" style="padding-left: 20px">연령대</th>
+              <th scope="col">성별</th>
+              <th scope="col">인구 수</th>
+              <th scope="col">비율</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="item in 4" :key="item">
+              <td style="padding-left: 20px">{{ population.age }}</td>
+              <td>{{ population.sex }}</td>
+              <td>{{ population.pop }}명</td>
+              <td>{{ population.ratio }}%</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+    <!--인구정보 끝-->
+    <hr style="background: #00b3d6; border: 0; height: 1.5px" />
+    <!--주요시설-->
+    <div>
+      <p style="font-size: large; font-weight: bold">
+        <span style="color: #00b3d6">{{ dong }}</span
+        ><span>의 주요시설</span>
+      </p>
+      <div
+        class="table-responsive card"
+        style="max-height: 300px; overflow-y: auto"
+      >
+        <table class="table mb-0">
+          <tbody>
+            <tr v-for="item in 10" :key="item">
+              <td style="padding-left: 20px">{{ office.name }}</td>
+              <td>{{ office.type }}</td>
+              <td>{{ office.address }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+    <!--주요시설 끝-->
+    <hr style="background: #00b3d6; border: 0; height: 1.5px" />
+    <!--학교-->
+    <div>
+      <p style="font-size: large; font-weight: bold">
+        <span style="color: #00b3d6">{{ dong }}</span
+        ><span>의 학교</span>
+      </p>
+      <div
+        class="table-responsive card"
+        style="max-height: 300px; overflow-y: auto"
+      >
+        <table class="table mb-0">
+          <tbody>
+            <tr v-for="item in 10" :key="item">
+              <td style="padding-left: 20px">{{ school.name }}</td>
+              <td>{{ school.type }}</td>
+              <td>{{ school.address }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+    <!--학교 끝-->
+    <hr style="background: #00b3d6; border: 0; height: 1.5px" />
+    <!--맛집-->
+    <div>
+      <p style="font-size: large; font-weight: bold">
+        <span style="color: #00b3d6">{{ dong }}</span
+        ><span>의 맛집</span>
+      </p>
+      <div
+        class="table-responsive card"
+        style="max-height: 300px; overflow-y: auto"
+      >
+        <table class="table mb-0">
+          <tbody>
+            <tr v-for="item in 20" :key="item">
+              <td style="padding-left: 20px">{{ restaurant.name }}</td>
+              <td>{{ restaurant.type }}</td>
+              <td>{{ restaurant.address }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+    <!--맛집 끝-->
+    <hr style="background: #00b3d6; border: 0; height: 1.5px" />
+    <!--CCTV-->
+    <div>
+      <p style="font-size: large; font-weight: bold">
+        <span style="color: #00b3d6">{{ dong }}</span
+        ><span>의 CCTV</span>
+      </p>
+      <div
+        class="table-responsive card"
+        style="max-height: 300px; overflow-y: auto"
+      >
+        <table class="table mb-0">
+          <tbody>
+            <tr v-for="item in 5" :key="item">
+              <td style="padding-left: 20px">{{ cctv }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+    <!--CCTV 끝-->
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.table-responsive.card::-webkit-scrollbar {
+  width: 10px;
+}
+.table-responsive.card::-webkit-scrollbar-thumb {
+  background: #f3f3f3;
+  border-radius: 10px;
+}
+</style>
--- front/zipkok/src/components/house/detail/item/HouseDetailRecItem.vue
+++ front/zipkok/src/components/house/detail/item/HouseDetailRecItem.vue
@@ -1,7 +1,136 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+const noImage = ref("/src/assets/noImage.jpg");
+const news = ref([
+  {
+    source: "매일경제",
+    title: "[컨콜 종합] 컴투스, 탄탄한 게임사업에 경영 효율화 더하니 ‘흑전’",
+    preview: "최고최고입니다.",
+    link: "https://n.news.naver.com/mnews/article/009/0005301504",
+    img: "/src/assets/newsSample.jpeg",
+    date: "2024.05.11",
+  },
+  {
+    source: "매일경제",
+    title: "[컨콜 종합] 컴투스, 탄탄한 게임사업에 경영 효율화 더하니 ‘흑전’",
+    preview: "최고최고입니다.",
+    link: "https://n.news.naver.com/mnews/article/009/0005301504",
+    img: null,
+    date: "2024.05.11",
+  },
+  {
+    source: "매일경제",
+    title: "[컨콜 종합] 컴투스, 탄탄한 게임사업에 경영 효율화 더하니 ‘흑전’",
+    preview: "최고최고입니다.",
+    link: "https://n.news.naver.com/mnews/article/009/0005301504",
+    img: "/src/assets/newsSample.jpeg",
+    date: "2024.05.11",
+  },
+  {
+    source: "매일경제",
+    title: "[컨콜 종합] 컴투스, 탄탄한 게임사업에 경영 효율화 더하니 ‘흑전’",
+    preview: "최고최고입니다.",
+    link: "https://n.news.naver.com/mnews/article/009/0005301504",
+    img: null,
+    date: "2024.05.11",
+  },
+  {
+    source: "매일경제",
+    title: "[컨콜 종합] 컴투스, 탄탄한 게임사업에 경영 효율화 더하니 ‘흑전’",
+    preview: "최고최고입니다.",
+    link: "https://n.news.naver.com/mnews/article/009/0005301504",
+    img: "/src/assets/newsSample.jpeg",
+    date: "2024.05.11",
+  },
+  {
+    source: "매일경제",
+    title: "[컨콜 종합] 컴투스, 탄탄한 게임사업에 경영 효율화 더하니 ‘흑전’",
+    preview: "최고최고입니다.",
+    link: "https://n.news.naver.com/mnews/article/009/0005301504",
+    img: null,
+    date: "2024.05.11",
+  },
+  {
+    source: "매일경제",
+    title: "[컨콜 종합] 컴투스, 탄탄한 게임사업에 경영 효율화 더하니 ‘흑전’",
+    preview: "최고최고입니다.",
+    link: "https://n.news.naver.com/mnews/article/009/0005301504",
+    img: null,
+    date: "2024.05.11",
+  },
+  {
+    source: "매일경제",
+    title: "[컨콜 종합] 컴투스, 탄탄한 게임사업에 경영 효율화 더하니 ‘흑전’",
+    preview: "최고최고입니다.",
+    link: "https://n.news.naver.com/mnews/article/009/0005301504",
+    img: "/src/assets/newsSample.jpeg",
+    date: "2024.05.11",
+  },
+  {
+    source: "매일경제",
+    title: "[컨콜 종합] 컴투스, 탄탄한 게임사업에 경영 효율화 더하니 ‘흑전’",
+    preview: "최고최고입니다.",
+    link: "https://n.news.naver.com/mnews/article/009/0005301504",
+    img: null,
+    date: "2024.05.11",
+  },
+  {
+    source: "매일경제",
+    title: "[컨콜 종합] 컴투스, 탄탄한 게임사업에 경영 효율화 더하니 ‘흑전’",
+    preview: "최고최고입니다.",
+    link: "https://n.news.naver.com/mnews/article/009/0005301504",
+    img: "/src/assets/newsSample.jpeg",
+    date: "2024.05.11",
+  },
+  {
+    source: "매일경제",
+    title: "[컨콜 종합] 컴투스, 탄탄한 게임사업에 경영 효율화 더하니 ‘흑전’",
+    preview: "최고최고입니다.",
+    link: "https://n.news.naver.com/mnews/article/009/0005301504",
+    img: null,
+    date: "2024.05.11",
+  },
+]);
+</script>
 
 <template>
-  <div>최신소식정보입니다.</div>
+  <div class="me-2">
+    <div class="card mb-3 w-100" v-for="item in news" :key="item">
+      <div class="row g-0">
+        <div class="col-md-4">
+          <img
+            :src="item.img == null ? noImage : item.img"
+            class="img-fluid rounded-start"
+            style="width: 100%; height: 100%"
+          />
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <small class="me-0" style="color: #00b3d6; font-weight: bold">
+              {{ item.source }}
+            </small>
+            <a :href="item.link"
+              ><h5 class="card-title">{{ item.title }}</h5></a
+            >
+            <p class="card-text">
+              {{ item.preview }}
+            </p>
+            <p class="card-text">
+              <small class="text-body-secondary">{{ item.date }}</small>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+/* .img-fluid.rounded-start {
+  object-fit: obtain;
+} */
+a {
+  text-decoration-line: none;
+  color: black;
+}
+</style>
