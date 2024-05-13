a96122f1d33afa82666d6ff3de995685d9f45388
ray <yr5698@gmail.com>
2024-05-14 01:18:06 +0900

    feat: 기본 지도 추가

--- front/zipkok/src/components/house/item/MapViewItem.vue
+++ front/zipkok/src/components/house/item/MapViewItem.vue
@@ -1,12 +1,13 @@
 <script setup>
+import { KakaoMap } from 'vue3-kakao-maps';
 
+
+console.log(KakaoMap)
 </script>
 
 <template>
-    <div>
-        mapview
-    </div>
-</template>
+    <KakaoMap :lat="33.450701" :lng="126.570667" />
+  </template>
 
 <style scoped>
 
--- front/zipkok/src/main.js
+++ front/zipkok/src/main.js
@@ -8,6 +8,12 @@ import router from './router'
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
 import "@/assets/css/main.css";
+import { useKakao } from 'vue3-kakao-maps/@utils';
+
+const { VITE_KAKAO_MAP_API_KEY } = import.meta.env;
+
+useKakao(VITE_KAKAO_MAP_API_KEY);
+console.log(VITE_KAKAO_MAP_API_KEY);
 const app = createApp(App)
 
 app.use(createPinia())
