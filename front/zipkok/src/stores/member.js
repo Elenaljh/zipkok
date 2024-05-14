14a41571be9c0d136ed5a63d2e539e1ae1825882
Jihye Lee <zerom980706@gmail.com>
2024-05-14 23:00:25 +0900

    feat: 회원관리 백엔드 api 통합

---
+++ front/zipkok/src/stores/member.js
@@ -0,0 +1,40 @@
+import { ref, computed } from "vue";
+import { defineStore } from "pinia";
+import axios from "axios";
+import { useRouter } from "vue-router";
+
+export const useMemberStore = defineStore("member", () => {
+  const router = useRouter();
+
+  const login = (email, password) => {
+    axios
+      .post("/login", {
+        email: email,
+        password: password,
+      })
+      .catch((error) => {
+        console.log(error.response);
+        console.log("로그인 실패");
+      });
+  };
+
+  const logout = () => {
+    axios.get("/logout");
+  };
+
+  const redirectLogin = () => {
+    alert("로그인이 필요합니다");
+    router.push({ name: "login" });
+  };
+
+  const authorizationRequest = () => {
+    axios.get("/authorization").catch(redirectLogin());
+  };
+
+  return {
+    login,
+    redirectLogin,
+    authorizationRequest,
+    logout,
+  };
+});
