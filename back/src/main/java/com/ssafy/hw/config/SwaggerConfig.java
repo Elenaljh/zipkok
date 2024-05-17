3999e044375e5d7ebc4afc62a1ff0f1271c6f724
Elenaljh <hye9807@naver.com>
2024-05-17 13:55:19 +0900

    swagger

--- back/src/main/java/com/ssafy/hw/config/SwaggerConfig.java
+++ back/src/main/java/com/ssafy/hw/config/SwaggerConfig.java
@@ -18,8 +18,8 @@ public class SwaggerConfig {
  
     private Info apiInfo() {
         return new Info()
-                .title("SSAFY 사용자 관리 API")
-                .description("<h3>워크샵에서 사용되는 RestApi에 대한 문서를 제공한다.</h3>")
+                .title("ZIPKOK API")
+                .description("<h3>집콕 api 문서</h3>")
                 .version("1.0.0");
     }
 }
